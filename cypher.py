e4119e2d17c6cb53b2f649bc281a29f7bf3d04bc
mrasamny <mrasamny@desu.edu>
2021-01-18 14:58:59 -0500

    initial commit

---
+++ cypher.py
@@ -0,0 +1,19 @@
+# Department of Computer and Information Sciences
+# CSCI-120 Elements of Computer Programming I
+# Skill Builder 7 - Encryption with a password
+# *******************************************************
+
+import string
+
+alphabet = string.printable
+lookup = {ch: i for i, ch in enumerate(alphabet)}
+
+
+def encrypt(message, password):
+    encrypted_message = ''
+    pass  # replace this line with your code.
+
+
+def decrypt(message, password):
+    decrypted_message = ''
+    pass  # replace this line with your code.
